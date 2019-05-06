70b396978c4794c7901f9e97e74682cee5b2c5da
chenxing <xing3987@163.com>
2019-05-07 00:53:26 +0800

    scala

--- src/demo/Helper.scala
+++ src/demo/Helper.scala
@@ -4,4 +4,8 @@ object Helper {
   def printlnSplit(): Unit = {
     println("*******华丽的分隔符***********")
   }
+
+  def printlnSplit(x: String): Unit = {
+    println(s"*${x}******华丽的分隔符***********")
+  }
 }
